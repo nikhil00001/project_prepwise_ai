1717ec1fcb369ad5a22c788a8bc231e0248a4343
nikhil <nikhilpandit123456@gmail.com>
2025-05-03 18:30:13 +0530

    setup app, file and folder structure, routing and authpages

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import AuthForm from '@/components/AuthForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <AuthForm type="sign-up" /> // Assuming AuthForm accepts a type prop
+  )
+}
+
+export default page
---
+++ components/FormField.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FormItem, FormLabel, FormControl, FormDescription, FormMessage } from './ui/form'
+import { Input } from './ui/input'
+import { Controller, FieldValues, Path, Control } from 'react-hook-form'
+
+interface FormFieldProps<T extends FieldValues> {
+	control: Control<T>
+	name: Path<T>
+	label: string
+	placeholder?: string
+	type?: 'text' | 'email' | 'password' | 'file'
+}
+
+const FormField = <T extends FieldValues>({ control, name, label, placeholder, type ="text"}: FormFieldProps<T>) => (
+		<Controller name={name} control={control} render={({ field, fieldState, formState }) => (
+			<FormItem>
+			    <FormLabel className='label'>{label}</FormLabel>
+				<FormControl>
+					<Input className='input' placeholder={placeholder} type={type} {...field} />
+				</FormControl>
+				<FormMessage />
+			</FormItem>
+		)}
+		/>
+);
+
+export { FormField as CustomFormField };
